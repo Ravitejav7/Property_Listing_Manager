7362351adba15bd2f8fd7cb66e2895cf305237b7
Ravitejav7 <vanjaraput.ug21.ec@nitp.ac.in>
2025-05-31 18:26:42 +0530

    updated Readme

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send(`Property Listing Manager API is running. Try /${process.env.APP_NAME}/property  to see data`);
+    res.send(
+      `Property Listing Manager API is running. Try /${process.env.APP_NAME}/property  to see data \n Note:  /${process.env.APP_NAME} is the base path for the API.`
+    );
 });
 //API ENTRY POINT
 app.use(`/${process.env.APP_NAME}`, router);
