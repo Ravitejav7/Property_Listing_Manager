f3cf8cfb23f5092016202dbd4160d592a0f2f32c
Ravitejav7 <vanjaraput.ug21.ec@nitp.ac.in>
2025-05-31 17:49:02 +0530

    created dev branch for local

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,12 @@ const server = createServer(app);
 
 server.listen(PORT, () => {
   try {
-    //console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
     createMongoConnection();
-    console.log(
-      `App deployed at :  https://property-listing-manager.onrender.com`
-    );
+   // console.log(`App deployed at :  https://property-listing-manager.onrender.com`);
+    console.log(`URL :  http://localhost:${PORT}/${process.env.APP_NAME}`);
+
+
   } catch (error) {
     console.error("Error starting server:", error);
     process.exit(1);
